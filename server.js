66ba36db516ab3935c239290d75a48f0fa84094d
Andrew-Shatokhin <114595292+Alina-Shatohina@users.noreply.github.com>
2023-05-21 00:36:57 +0300

    move db_host to procces

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const DB_HOST = "mongodb+srv://Alina:bq7lS3JpEneMM6YE@cluster0.m74ok6b.mongodb.net/db-contacts?retryWrites=true&w=majority"
-mongoose.connect(DB_HOST)
+const { DB_HOST } = process.env;
+
+
+mongoose.connect(DB_HOST )
   .then(() => {
       app.listen(3000, () => {
       console.log("Database connection successful")
