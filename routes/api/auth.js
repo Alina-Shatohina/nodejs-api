830accf03675d94c299407175bf07ca48501d3c7
Andrew-Shatokhin <114595292+Alina-Shatohina@users.noreply.github.com>
2023-05-26 20:07:16 +0300

    add login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,7 @@ const ctrl = require("../../controllers/auth");
 const { validateBody } = require("../../decorators");
 
 const { schemas } = require("../../models/user");
+const { Schema } = require("mongoose");
 
 const router = express.Router();
 
@@ -12,5 +13,6 @@ const router = express.Router();
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 module.exports = router;
