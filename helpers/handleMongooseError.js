f2d6002a3372f6b1e5484f0cc8fe14b4afd7a8a5
Andrew-Shatokhin <114595292+Alina-Shatohina@users.noreply.github.com>
2023-05-26 19:05:04 +0300

    add 409 error for register
    (unique email)

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,9 +1,12 @@
 const handleMongooseError = (error, data, next) => {
-    error.status = 404;
+    const { name, code } = error;
+    
+    const status = (name === "MongoServerError" && code === 11000) ? 409 : 400;
+    error.status = status;
     next()
 };
 
 
 module.exports = {
-    handleMongooseError
+    handleMongooseError,
 }
